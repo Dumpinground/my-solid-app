d78f206801404e91ab353069487f59450676de06
Dumpinground <e605336584@icloud.com>
2023-02-10 12:11:51 +0000

    Update 6 files

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Component } from 'solid-js';
 import logo from './logo.svg';
 import styles from './App.module.css';
 
-
 import { CountingComponent } from "./Components/Counter"
 import { TodoList } from "./Components/Simple Todos"
 
@@ -19,6 +18,9 @@ const App: Component = () => {
     <div class={styles.App}>
       <header class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
+        
+        <Hello name="Solidjs"/>
+        
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
